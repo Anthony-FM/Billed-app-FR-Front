93e6e5dc1716e4af287d57654cc91c0c61a29e35
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-01-11 16:05:53 +0100

    :bug: Correction du 3eme bug file.type

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -15,9 +15,11 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+  
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const imagesMIMEsTypeAccepted = ["image/jpeg", "image/jpg", "image/png"]
+    const file = this.document.querySelector(`input[data-testid="file"]`).files[0] 
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
@@ -25,20 +27,31 @@ export default class NewBill {
     formData.append('file', file)
     formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    if(imagesMIMEsTypeAccepted.includes(file.type) === false ){
+      this.document.querySelector(".message").classList.remove("hidden")
+      this.document.querySelector(`input[data-testid="file"]`).focus();
+      this.document.querySelector(`input[data-testid="file"]`).value = ''
+      
+    } else {
+      this.document.querySelector(".message").classList.remove("hidden")
+      this.document.querySelector(`.message`).textContent = "";
+      
+        this.store
+          .bills()
+          .create({
+            data: formData,
+            headers: {
+              noContentType: true
+            }
+          })
+          .then(({fileUrl, key}) => {
+            this.billId = key
+            this.fileUrl = fileUrl
+            this.fileName = fileName
+          }).catch(error => console.error(error))
+      
+    }
+    
   }
   handleSubmit = e => {
     e.preventDefault()
@@ -57,6 +70,7 @@ export default class NewBill {
       fileName: this.fileName,
       status: 'pending'
     }
+    console.log(bill)
     this.updateBill(bill)
     this.onNavigate(ROUTES_PATH['Bills'])
   }
