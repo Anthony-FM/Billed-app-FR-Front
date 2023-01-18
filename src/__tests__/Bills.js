5c96a2d5210f7a81c7bb6fecb666dc9b9717d605
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-01-18 16:08:16 +0100

    Correction premier Test + Ajout second Test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,18 +2,17 @@
  * @jest-environment jsdom
  */
 
-import Bills from "../containers/Bills.js";
-import { ROUTES, ROUTES_PATH } from "../constants/routes"
 
 import {screen, waitFor} from "@testing-library/dom";
 import userEvent from '@testing-library/user-event'
 import "@testing-library/jest-dom/extend-expect";
+
+import Bills from "../containers/Bills.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
 import router from "../app/Router.js";
-import NewBill from "../containers/NewBill.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -70,26 +69,53 @@ describe("Given I am connected as an employee", () => {
       }
       window.onNavigate(ROUTES_PATH.Bills)
 
-
-
-      const bills1 = new Bills({
-        document, onNavigate, store: null, localStorage: window.localStorage
+      const bill = new Bills({
+        document, onNavigate, store: null, bills:bills, localStorage: window.localStorage
       })
-      document.body.innerHTML = BillsUI({ data:  [bills[0]]  })
+      
+      document.body.innerHTML = BillsUI({ data:[bills[0]] })
       const eyeIcon = screen.getByTestId('icon-eye')
 
-      // $.fn.modal = jest.fn();
-      // const handleClickIconEye = jest.fn(bills1.handleClickIconEye())
-      
-      const handleClickIconEye1=jest.fn((e)=>bills1.handleClickIconEye(e,eyeIcon[0]))
+      const handleClickIconEye1=jest.fn(bill.handleClickIconEye(eyeIcon))      
       
       userEvent.click(eyeIcon)
-      eyeIcon.addEventListener('click', handleClickIconEye1)      
+      eyeIcon.addEventListener('click', handleClickIconEye1())      
       expect(handleClickIconEye1).toHaveBeenCalled()
 
       const modalEmployee = screen.getByTestId('modalEmployee')
       expect(modalEmployee).toBeTruthy()
 
+    })
+    test("Then on click the function should be called", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      window.onNavigate(ROUTES_PATH.NewBill)
+
+      const bill = new Bills({
+        document, onNavigate, store: null, bills:bills, localStorage: window.localStorage
+      })
+      
+      document.body.innerHTML = BillsUI({ data:bills })
+      
+      const buttonNewBill = screen.getByTestId("btn-new-bill")
+
+      const handleClickNewBill=jest.fn(bill.handleClickNewBill())
+      
+      userEvent.click(buttonNewBill)
+      buttonNewBill.addEventListener('click', handleClickNewBill()) 
+
+      expect(handleClickNewBill).toHaveBeenCalled()
+      
     })
   })
 })
